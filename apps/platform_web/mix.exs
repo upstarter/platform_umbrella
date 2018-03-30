46b99c33a6c8ec54c5c538f3c13c189c56c68f06
Eric Steen <ericsteen1@gmail.com>
2018-03-30 16:19:07 -0700

    Fix prod secrets issue

--- apps/platform_web/mix.exs
+++ apps/platform_web/mix.exs
@@ -37,7 +37,6 @@ defmodule PlatformWeb.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:distillery, "~> 1.5"},
       {:phoenix, "~> 1.3.1"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
