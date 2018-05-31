c3c7d4937d17a03c2dbb328c58f0fcafdafcbf05
Eric Steen <ericsteen1@gmail.com>
2018-05-30 21:08:00 -0700

    Chatbot

--- apps/platform_web/assets/js/app.js
+++ apps/platform_web/assets/js/app.js
@@ -35,3 +35,12 @@ window.onloadCallback = () => {
 analytics.load("DZkRyiXKnNGmbh2EWo1LT78RPyhnVyFy");
 analytics.page();
 }}();
+
+// FB
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
