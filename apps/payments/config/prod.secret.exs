7e1823c3b3cfa016ed92f1b209125525a594174a
Eric Steen <ericsteen1@gmail.com>
2018-06-11 13:28:48 -0700

    socket to host

--- apps/payments/config/prod.secret.exs
+++ apps/payments/config/prod.secret.exs
@@ -15,5 +15,5 @@ config :payments, Payments.Repo,
   username: System.get_env("POSTGRES_USER"),
   password: System.get_env("POSTGRES_PASSWORD"),
   database: "payments_prod",
-  socket: System.get_env("POSTGRES_SOCKET_PATH"),
+  host: System.get_env("POSTGRES_SOCKET_PATH"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
