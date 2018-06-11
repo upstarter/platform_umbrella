8ee3f8470ce8800f3f36a85f5b52b9cb8d297c9f
Eric Steen <ericsteen1@gmail.com>
2018-06-11 11:53:52 -0700

    Fix config

--- config/prod.exs
+++ config/prod.exs
@@ -3,11 +3,10 @@ use Mix.Config
 config :platform_umbrella, Platform.Umbrella.Endpoint,
   load_from_system_env: true,
   http: [port: {:system, "PORT"}],
-  url: [host: "eternal-sunset-206422.appspot.com", port: {:system, "PORT"}], # This is critical for ensuring web-sockets properly authorize.
+  # url: [host: "eternal-sunset-206422.appspot.com", port: {:system, "PORT"}], # This is critical for ensuring web-sockets properly authorize.
   check_origin: false,
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
-  root: ".",
-  version: Application.spec(:myapp, :vsn)
+  root: "."
 # Do not print debug messages in production
 config :logger, level: :info
