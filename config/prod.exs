7b8009e930819cf227fa4408f697651b12847f90
Eric Steen <ericsteen1@gmail.com>
2018-06-11 16:21:39 -0700

    Add back dockerfile

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :platform_umbrella, Platform.Umbrella.Endpoint,
   load_from_system_env: true,
   http: [port: {:system, "PORT"}],
-  # url: [host: "eternal-sunset-206422.appspot.com", port: {:system, "PORT"}], # This is critical for ensuring web-sockets properly authorize.
+  url: [host: "cryptowise.ai", port: {:system, "PORT"}], # This is critical for ensuring web-sockets properly authorize.
   check_origin: false,
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
