4a21ceb08d267d8ebfc5397515de93e77be9c2ed
Eric Steen <ericsteen1@gmail.com>
2018-07-02 10:43:27 -0700

    Fixit

--- apps/platform_web/lib/platform_web/views/page_view.ex
+++ apps/platform_web/lib/platform_web/views/page_view.ex
@@ -2,14 +2,14 @@ defmodule PlatformWeb.PageView do
   use PlatformWeb, :view
 
   def team do
-    
+
   end
 
   def blog_posts do
     # url = "https://medium.com/@eric_11842/latest?format=json"
     # HTTPoison.get(url)
 
-    url = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F/@eric_11842"
+    url = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F/@cryptowise"
     GetMedium.Full.blog_posts(url, raw: true)
   end
 
