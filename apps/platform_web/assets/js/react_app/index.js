79307360785415e93934b12eb196bc29c65573be
Eric Steen <ericsteen1@gmail.com>
2018-07-09 21:48:10 -0700

    Fix

--- apps/platform_web/assets/js/react_app/index.js
+++ apps/platform_web/assets/js/react_app/index.js
@@ -8,7 +8,23 @@ import React from "react"
 import ReactDOM from "react-dom"
 import NavContainer from "./src/common/nav/nav"
 import HomeContainer from "./src/home/home"
+import PressContainer from "./src/press/press"
+import BlogContainer from "./src/blog/blog"
 
-ReactDOM.render(
-  <HomeContainer/>, document.getElementById("crypto-wise-app")
-)
+if (document.getElementById("crypto-wise-app")) {
+  ReactDOM.render(
+    <HomeContainer/>, document.getElementById("crypto-wise-app")
+  )
+}
+
+if (document.getElementById("press")) {
+  ReactDOM.render(
+    <PressContainer/>, document.getElementById("press")
+  )
+}
+
+if (document.getElementById("blog")) {
+  ReactDOM.render(
+    <BlogContainer/>, document.getElementById("blog")
+  )
+}
---
+++ apps/platform_web/assets/js/react_app/src/blog/BlogComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default class BlogComponent extends React.Component {
+  render() {
+    return (
+      
+    )
+  }
+}
