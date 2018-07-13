0980b141ea8003a0b1856e3fd5f772cbefcb3c05
Eric Steen <ericsteen1@gmail.com>
2018-07-13 12:24:00 -0700

    Fx

--- apps/platform_web/config/prod.exs
+++ apps/platform_web/config/prod.exs
@@ -16,11 +16,7 @@ use Mix.Config
 config :platform_web, PlatformWeb.Endpoint,
   load_from_system_env: true,
   http: [port: "${PORT}"],
-  url: [scheme: "https", host: "cryptowise.ai", port: 443],
-  force_ssl: [
-    host: nil,
-    rewrite_on: [:x_forwarded_proto]
-  ],
+  url: [host: "cryptowise.ai", port: 80],
   secret_key_base: "zSs42WIom2Vw/5MxHUfi+lqgOBFp0D1n4fJeHxaZ9yYAb9EzzoepurcxCXq3IBha",
   check_origin: false,
   server: true,
