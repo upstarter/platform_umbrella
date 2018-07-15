b1c67b1514b5cf2c271b6a512528046fbf19b65f
Eric Steen <ericsteen1@gmail.com>
2018-07-15 15:23:32 -0700

    Styling

--- apps/platform_web/assets/js/react_app/src/common/nav/NavContainer.js
+++ apps/platform_web/assets/js/react_app/src/common/nav/NavContainer.js
@@ -13,15 +13,16 @@ export default class NavContainer extends React.Component {
           <Link to="/" className="navbar-item .nav-logo">
             <img src={img} alt="CryptoWise: Cryptoasset research & analysis" width="112" height="28"></img>
           </Link>
-          <Link to="/" className="navbar-item">Home</Link>
-          <Link to="/press" className="navbar-item">Press</Link>
-          <Link to="/blog" className="navbar-item">Blog</Link>
-
           <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
            <span aria-hidden="true"></span>
            <span aria-hidden="true"></span>
            <span aria-hidden="true"></span>
           </a>
+        </div>
+        <div className="navbar-menu" id="navMenu">
+          <Link to="/" className="navbar-item">Home</Link>
+          <Link to="/press" className="navbar-item">Press</Link>
+          <Link to="/blog" className="navbar-item">Blog</Link>
         </div>
       </nav>
     )
