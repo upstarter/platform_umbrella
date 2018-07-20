2ebf1e18f2b692f302a2138f657a95ea3c5f8191
Eric Steen <ericsteen1@gmail.com>
2018-07-20 12:25:01 -0700

    Hero issues

---
+++ apps/platform_web/assets/js/react_app/src/home/HeroComponent.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Elm from 'react-elm-components'
+import {Main} from '../../../elm/src/Main'
+
+
+export default class HeroComponent extends React.Component {
+  render() {
+    return (
+      <section className="heero">
+        <div className="heero-body column">
+          <div className="container">
+            <h1 className="title">
+              Wiser cryptoasset investing starts with the ecosystem.
+            </h1>
+            <p className="subtitle-big">
+              Receive free investing insights & join the conversation.
+            </p>
+            <div className="scroll-to">
+              <a className="icon">
+                <i className="fa fa-chevron-down"></i>
+              </a>
+            </div>
+            <div id="subscribe-form" className="email-leadgen">
+              <section className="section">
+                <div className="container has-text-centered">
+                  <div id="form-container">
+                    <Elm src={Main} />
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
--- apps/platform_web/assets/js/react_app/src/home/HomeComponent.jsx
+++ apps/platform_web/assets/js/react_app/src/home/HomeComponent.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import HeroComponent from "../home/HeroComponent"
 import FeaturesComponent from "../home/FeaturesComponent"
 import Elm from 'react-elm-components'
 import {Main} from '../../../elm/src/Main'
@@ -9,32 +10,7 @@ export default class HomeComponent extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <section className="heero is-small">
-          <div className="heero-body">
-            <div className="container">
-              <h1 className="title">
-                Wiser cryptoasset investing starts with the ecosystem.
-              </h1>
-              <p className="subtitle-big">
-                Receive free investing insights & join the conversation.
-              </p>
-              <div className="scroll-to">
-                <a className="icon">
-                  <i className="fa fa-chevron-down"></i>
-                </a>
-              </div>
-              <div id="subscribe-form" className="email-leadgen">
-                <section className="section">
-                  <div className="container has-text-centered">
-                    <div id="form-container">
-                      <Elm src={Main} />
-                    </div>
-                  </div>
-                </section>
-              </div>
-            </div>
-          </div>
-        </section>
+        <HeroComponent />
         <FeaturesComponent />
       </React.Fragment>
     )
