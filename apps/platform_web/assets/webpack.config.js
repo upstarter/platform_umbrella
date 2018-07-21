cbf57e1f16737e51165df865140142bdf056ff6b
Eric Steen <ericsteen1@gmail.com>
2018-07-20 18:01:28 -0700

    Cleanup

--- apps/platform_web/assets/webpack.config.js
+++ apps/platform_web/assets/webpack.config.js
@@ -3,13 +3,13 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const env = process.env.MIX_ENV || "dev";
-const isProduction = env === "prod";
+const isProd = env === "prod";
 
 const elmSource = __dirname + "/js/elm";
 
 const prodElm = "/app/apps/platform_web/assets"
 const elmMake = "/node_modules/elm/binwrappers/elm-make"
-const elmMakePath = isProduction ?  prodElm + elmMake : __dirname + elmMake
+const elmMakePath = isProd ?  prodElm + elmMake : __dirname + elmMake
 
 module.exports = {
   devtool: "source-map",
@@ -38,7 +38,7 @@ module.exports = {
             {
               loader: "sass-loader",
               options: {
-                sourceComments: !isProduction
+                sourceComments: !isProd
               }
             }
           ]
@@ -53,7 +53,7 @@ module.exports = {
         test: /\.elm$/,
         exclude: ["/elm-stuff/", "/node_modules"],
         loader: "elm-webpack-loader",
-        options: { pathToMake: elmMakePath, maxInstances: 2, debug: true, warn: true, cwd: elmSource }
+        options: { pathToMake: elmMakePath, maxInstances: 2, debug: isProd, warn: true, cwd: elmSource }
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
@@ -71,135 +71,3 @@ module.exports = {
     new CopyWebpackPlugin([{ from: "./static" }])
   ]
 };
-
-// const path = require('path');
-// var ExtractTextPlugin = require("extract-text-webpack-plugin");
-// var merge = require("webpack-merge");
-// var webpack = require("webpack");
-//
-// var env = process.env.NODE_ENV || "development";
-// var production = env === "production";
-//
-// var node_modules_dir = "node_modules"
-//
-// var plugins = [
-//   new ExtractTextPlugin("./css/app.scss")
-// ]
-// // var plugins = [
-// //   new ExtractTextPlugin({
-// //     filename: "[name].[contenthash].css",
-// //     disable: process.env.NODE_ENV === "development"
-// //   });
-// // ]
-//
-// if (production) {
-//   plugins.push(
-//     new webpack.optimize.UglifyJsPlugin({
-//       compress: {warnings: false},
-//       output: {comments: false}
-//     })
-//   );
-// } else {
-//   plugins.push(
-//     new webpack.EvalSourceMapDevToolPlugin()
-//   );
-// }
-//
-// var common = {
-//   watchOptions: {
-//     poll: false
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(jsx?)/,
-//         exclude: [node_modules_dir],
-//         loader: "babel-loader",
-//         options: {
-//           presets: ["react", "es2015"]
-//         }
-//       },
-//       {
-//         test: /\.elm$/,
-//         exclude: [/elm-stuff/, /node_modules/],
-//         use: {
-//           loader: 'elm-webpack-loader',
-//           options: {cwd: 'js/elm'}
-//         }
-//       },
-//       {
-//         test: /\.scss$/,
-//         use: [
-//           {
-//             loader: 'style-loader'
-//           },
-//           {
-//             loader: 'css-loader'
-//           },
-//           {
-//             loader: 'sass-loader'
-//           }
-//         ]
-//       },
-//       // {
-//       //   test: /\.scss$/,
-//       //   use: ExtractTextPlugin.extract({
-//       //     fallback: 'style-loader',
-//       //     use: [
-//       //       // {
-//       //       //   loader: 'css-loader',
-//       //       // },
-//       //       // {
-//       //       //   loader: 'postcss-loader',
-//       //       //   options: {
-//       //       //     plugins() {
-//       //       //       return [
-//       //       //         require("precss"),
-//       //       //         require("autoprefixer")
-//       //       //       ];
-//       //       //     }
-//       //       //   }
-//       //       // },
-//       //       {
-//       //         loader: 'sass-loader',
-//       //         options: {
-//       //           // includePaths: ["css"]
-//       //         }
-//       //       }
-//       //     ]
-//       //   })
-//       // },
-//       {
-//         test: /\.(jpe?g|png|gif|svg)$/i,
-//         loader: "url-loader?name=/images/[name].[ext]",
-//       },
-//       {
-//         test: /\.(ttf|otf|eot|svg|woff2?)$/,
-//         loader: "file-loader?name=/fonts/[name].[ext]",
-//       }
-//     ],
-//     noParse: [/\.elm$/]
-//   },
-//   plugins: plugins
-// };
-//
-// module.exports = [
-//   merge(common, {
-//     entry: [
-//       __dirname + '/css/app.scss',
-//       __dirname + '/js/app.js',
-//       __dirname + '/js/elm/src/Main.elm'
-//     ],
-//     output: {
-//       path: __dirname + '../priv/static/js/',
-//       filename: "[name].js"
-//     },
-//     resolve: {
-//       modules: [
-//         __dirname + 'node_modules',
-//         __dirname + 'js'
-//       ],
-//       extensions: [".js", ".jsx", ".elm"]
-//     }
-//   })
-// ];
