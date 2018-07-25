6f54889411fe02ffbd8b42b4c1e9198776ec3ba4
Eric Steen <ericsteen1@gmail.com>
2018-07-25 08:42:03 -0700

    Fix

--- apps/platform_web/assets/js/react_app/src/providers/ProviderComponent.jsx
+++ apps/platform_web/assets/js/react_app/src/providers/ProviderComponent.jsx
@@ -5,13 +5,13 @@ export default class ProviderComponent extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <section id="normal-wrap" className="provider-contribute-wrapper container">
+        <section id="normal-wrap" className="provider-contribute-wrapper">
           <div className="scroll-to is-hidden-desktop">
             <a className="icon">
               <i className="fa fa-chevron-down"></i>
             </a>
           </div>
-           <div id="provider-text">
+           <div id="provider-text" className="text">
             <h1 className="provider-heading">
               Seeking Cryptoasset Experts
             </h1>
@@ -19,11 +19,11 @@ export default class ProviderComponent extends React.Component {
               We're building the Dow Jones of Cryptoassets and we're seeking
               high quality finance and crypto article submissions. Join our
               wise, talented, and passionate network of crypto analysts, researchers,
-              crypto-economists and industry experts who are focused on helping to form
-              the future of crypto technology by sharing insights and ideas. Our
-              editors curate news, research, opinion and discussion on key
-              topics of interest to crypto investors in order to help them make
-              better financial decisions.
+              crypto-economists and industry experts who are focused on forming
+              the future of crypto technology, filtering fraud, and who love
+              sharing insights and ideas. Our editors curate news, research,
+              opinion and discussion on key topics of interest to crypto investors
+              in order to help them make better financial decisions.
             </p>
             <br />
             <p>
@@ -34,29 +34,29 @@ export default class ProviderComponent extends React.Component {
             <br/>
             <h3>How it Works</h3>
             <p>
-              Submit your email address above if you are uniquely qualified to
-              contribute articles on the topics in our universe of discourse in
+              Submit your email address above if you feel you are uniquely qualified to
+              contribute articles on the below topics within
               the context of cryptoassets. We will send you a link to
               our contributor application. After you apply we will get back to
               you if there is a good fit.
             </p>
             <br/>
-            <h3 className="list-heading">Our universe of discourse:</h3>
+            <h3 className="list-heading">Our Universe of Discourse:</h3>
 
-            <div className="topics columns subtitle-big">
+            <div className="topics columns list">
               <div className="crypto-finance column">
                 <ul>
                   <li className="topic">Cryptoeconomics</li>
                   <li className="topic">Decentralized Apps (Dapps)</li>
                   <li className="topic">Cryptocurrencies</li>
-                  <li className="topic">ERC-XX</li>
                   <li className="topic">Regulatory</li>
                   <li className="topic">Compliance</li>
                   <li className="topic">CryptoCloud Compute</li>
                   <li className="topic">CryptoCloud Storage</li>
                   <li className="topic">Sustainability</li>
-                  <li className="topic">Transformative Tech</li>
+                  <li className="topic">Network Ownership Effects</li>
                   <li className="topic">Social Impact</li>
+                  <li className="topic">ERC-XX</li>
                 </ul>
               </div>
               <div className="emerging-tech column">
