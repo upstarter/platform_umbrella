91106e0b05c4a9d2f07b6269fb7740fee2865ca3
Eric Steen <ericsteen1@gmail.com>
2018-07-30 15:47:41 -0700

    remove all apps but platform

--- apps/auth/lib/auth/current_user.ex
+++
@@ -1,9 +0,0 @@
-defmodule Auth.CurrentUser do
-  import Plug.Conn
-  import Guardian.Plug
-  def init(opts), do: opts
-  def call(conn, _opts) do
-    current_user = current_resource(conn)
-    assign(conn, :current_user, current_user)
-  end
-end
--- apps/users/config/config.exs
+++
@@ -1,5 +0,0 @@
-use Mix.Config
-
-config :users, ecto_repos: [Users.Repo]
-
-import_config "#{Mix.env}.exs"
