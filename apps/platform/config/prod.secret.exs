40cb1ed0ba4e7f89abbd02e6823a3700af84b842
Eric Steen <ericsteen1@gmail.com>
2018-07-30 15:59:12 -0700

    Fix pool size

--- apps/platform/config/prod.secret.exs
+++ apps/platform/config/prod.secret.exs
@@ -16,4 +16,4 @@ config :platform, Platform.Repo,
   password: System.get_env("POSTGRES_PASSWORD"),
   database: "platform_prod",
   socket: System.get_env("POSTGRES_SOCKET_PATH"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "50")
