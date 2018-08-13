45bf9d26ce806536b86cd9a01101a59c6f112b53
Eric Steen <ericsteen1@gmail.com>
2018-08-12 17:05:07 -0700

    Users

---
+++ apps/platform/lib/platform/users/user.ex
@@ -0,0 +1,20 @@
+defmodule Platform.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :password, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :password])
+    |> validate_required([:name, :email, :password])
+  end
+end
