45097bcb8a44111c6188935f28e122078a6bc0ba
Eric Steen <ericsteen1@gmail.com>
2018-08-15 10:11:36 -0700

    Add portfolio endpoint json at /api/v1/portfolio '

--- apps/platform_web/assets/js/react_app/src/components/portfolio/PortfolioComponent.js
+++ apps/platform_web/assets/js/react_app/src/components/portfolio/PortfolioComponent.js
@@ -1,7 +1,42 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import {url} from '../../utils/consts'
 
 export default class PortfolioComponent extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      tokens: [],
+      selectedToken: null,
+      isLoading: true,
+      error: null
+    };
+  }
+
+
+  componentDidMount() {
+    console.log(`${url}/api/v1/portfolio`);
+
+      this.setState({ isLoading: true });
+
+      fetch(`${url}/api/v1/portfolio`)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error('Something went wrong ...');
+          }
+        })
+        .then(data => this.setState({
+          blogPosts: data.portfolio,
+          selectedPost: data.portfolio[0],
+          isLoading: false
+        }))
+        .catch(error => this.setState({ error, isLoading: false }));
+    }
+
   render() {
     return (
       <React.Fragment>
--- apps/platform_web/lib/platform_web/views/providers/portfolio_view.ex
+++
@@ -1,5 +0,0 @@
-defmodule PlatformWeb.Providers.PortfolioView do
-  use PlatformWeb, :view
-
-
-end
