3222ffb4916a4a7348634aab841c5d6ddc02c072
Eric Steen <ericsteen1@gmail.com>
2018-08-21 19:40:51 -0700

    Cleanup redux

--- apps/platform_web/assets/js/react_app/src/actions/index.js
+++ apps/platform_web/assets/js/react_app/src/actions/index.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
-// const ROOT_URL = `http://localhost:4000`;
+const ROOT_URL = `http://localhost:4000`;
 
 export const FETCH_TOKENS = "FETCH_TOKENS";
 
 // action creator
-export default function fetchTokens(term) {
-  // const url = `${ROOT_URL}/api/v1/tokens?q=${term}`;
-  // const request = axios.get(url);
+export function fetchTokens(term) {
+  const url = `${ROOT_URL}/api/v1/tokens?q=${term}`;
+  const request = axios.get(url);
 
+  console.log('Action Received: ', request)
   return {
     type: FETCH_TOKENS,
-    payload: []
+    payload: request
   };
 }
