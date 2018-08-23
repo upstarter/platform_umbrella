768eef5b6d31de92f96bb073c35a8fa9b38ab362
Tuguldur Bayarnyam <tuguscript@gmail.com>
2018-08-23 14:31:16 -0700

    implemented navbar that functions on every page (#13)

--- apps/platform_web/assets/js/react_app/src/components/portfolio/PortfolioContainer.js
+++ apps/platform_web/assets/js/react_app/src/components/portfolio/PortfolioContainer.js
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import NavContainer from "../nav/nav";
 import ProviderHeroComponent from "../../providers/ProviderHeroComponent"
 import PortfolioComponent from "./PortfolioComponent"
 
@@ -8,7 +7,6 @@ export default class PortfolioContainer extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <NavContainer url="/contribute"/>
         <PortfolioComponent />
       </React.Fragment>
     )
--- apps/platform_web/assets/js/react_app/src/providers/ProviderContainer.js
+++ apps/platform_web/assets/js/react_app/src/providers/ProviderContainer.js
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import NavContainer from "../components/nav/nav";
 import ProviderHeroComponent from "./ProviderHeroComponent"
 import ProviderComponent from "./ProviderComponent"
 
@@ -8,7 +7,6 @@ export default class ProviderContainer extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <NavContainer url="/contribute"/>
         <ProviderHeroComponent />
         <ProviderComponent />
       </React.Fragment>
