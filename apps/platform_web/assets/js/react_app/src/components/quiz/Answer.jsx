9ad38c17da720b560c3333024c803f3a0e7b9af6
tuguscript <tuguscript@gmail.com>
2018-08-28 14:17:02 -0700

    created components that will be needed for quiz

---
+++ apps/platform_web/assets/js/react_app/src/components/quiz/Answer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Answer extends Component {
+  render() {
+    return (
+      <div>
+        answer
+      </div>
+    )
+  }
+}
+
+export default Answer
