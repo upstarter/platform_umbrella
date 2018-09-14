2143617fad840eb63e2699ddbcc5ba4864d32e28
tuguscript <tuguscript@gmail.com>
2018-09-14 16:04:44 -0700

    created token table

--- apps/platform_web/assets/js/react_app/src/components/portfolio/PortfolioComponent.js
+++ apps/platform_web/assets/js/react_app/src/components/portfolio/PortfolioComponent.js
@@ -1,12 +1,9 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import {url} from '../../utils/consts'
-import PortfolioGrid from '../datagrid/PortfolioGrid'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { url } from "../../utils/consts";
+import PortfolioGrid from "../datagrid/PortfolioGrid";
 
 export default class PortfolioComponent extends React.Component {
-
-
   constructor(props) {
     super(props);
     this.state = {
@@ -17,48 +14,56 @@ export default class PortfolioComponent extends React.Component {
     };
   }
 
-
   componentDidMount() {
     console.log(`${url}/api/v1/portfolios/new`);
 
-      this.setState({ isLoading: true });
+    this.setState({
+      isLoading: true
+    });
 
-      fetch(`${url}/api/v1/portfolios/new`)
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error('Something went wrong ...');
-          }
-        })
-        .then(data => this.setState({
+    fetch(`${url}/api/v1/portfolios/new`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Something went wrong ...");
+        }
+      })
+      .then(data =>
+        this.setState({
           blogPosts: data.portfolio,
           selectedPost: data.portfolio[0],
           isLoading: false
-        }))
-        .catch(error => this.setState({ error, isLoading: false }));
-    }
+        })
+      )
+      .catch(error =>
+        this.setState({
+          error,
+          isLoading: false
+        })
+      );
+  }
 
   render() {
     return (
       <React.Fragment>
         <section id="portfolio" className="light-wrap">
-
-           <div id="portfolio-text" className="content">
+          <div id="portfolio-text" className="content">
             <h1 className="title portfolio-heading center">
-              The CryptoWise Collaborative Portfolio (CCP)
-            </h1>
+              The CryptoWise Collaborative Portfolio(CCP){" "}
+            </h1>{" "}
             <h5 className="content center">
-              Curated by the wisest minds in crypto. Enter your portfolio allocation
-              and we will send you regular wisdom of the crowd updates.
-            </h5>
+              Curated by the wisest minds in crypto.Enter your portfolio
+              allocation and we will send you regular wisdom of the crowd
+              updates.{" "}
+            </h5>{" "}
             <br />
             <div className="portfolio-grid center">
-              <PortfolioGrid/>
-            </div>
-          </div>
-        </section>
+              <PortfolioGrid />
+            </div>{" "}
+          </div>{" "}
+        </section>{" "}
       </React.Fragment>
-    )
+    );
   }
 }
