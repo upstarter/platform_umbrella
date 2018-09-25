c2c2ee517121365521a2ec9e3bc3464822222256
Eric Steen <ericsteen1@gmail.com>
2018-09-25 11:01:11 -0700

    Add antd

--- apps/platform_web/assets/js/react_app/src/components/nav/NavContainer.js
+++ apps/platform_web/assets/js/react_app/src/components/nav/NavContainer.js
@@ -18,57 +18,57 @@ class NavContainer extends React.Component {
     this.hideBar = this.hideBar.bind(this);
   }
   componentDidMount() {
-    window.addEventListener("scroll", this.hideBar);
-    // -- Navbar menu JS
-    window.addEventListener("DOMContentLoaded", () => {
-      // Get all "navbar-burger" elements
-      const $navbarBurgers = Array.prototype.slice.call(
-        document.querySelectorAll(".navbar-burger"),
-        0
-      );
-
-      // Check if there are any navbar burgers
-      if ($navbarBurgers.length > 0) {
-        // Add a click event on each of them
-        $navbarBurgers.forEach(burger => {
-          burger.addEventListener("click", () => {
-            // Get the target from the "data-target" attribute
-            const target = burger.dataset.target;
-            const $target = document.getElementById(target);
-            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-            burger.classList.toggle("is-active");
-            $target.classList.toggle("is-active");
-          });
-        });
-      }
-    });
-
-    const $navbarLinks = Array.prototype.slice.call(
-      document.querySelectorAll(".navbar-menu div a"),
-      0
-    );
-    const $navMenu = $navbarLinks[0].parentElement.parentElement;
-
-    $navbarLinks.forEach(link => {
-      link.addEventListener("click", () => {
-        const $burger = document.querySelector(".navbar-burger");
-
-        // Toggle the "is-active" class on burger & "navbar-menu"
-        $burger.classList.toggle("is-active");
-        $navMenu.classList.toggle("is-active");
-      });
-    });
+    // window.addEventListener("scroll", this.hideBar);
+    // // -- Navbar menu JS
+    // window.addEventListener("DOMContentLoaded", () => {
+    //   // Get all "navbar-burger" elements
+    //   const $navbarBurgers = Array.prototype.slice.call(
+    //     document.querySelectorAll(".navbar-burger"),
+    //     0
+    //   );
+    //
+    //   // Check if there are any navbar burgers
+    //   if ($navbarBurgers.length > 0) {
+    //     // Add a click event on each of them
+    //     $navbarBurgers.forEach(burger => {
+    //       burger.addEventListener("click", () => {
+    //         // Get the target from the "data-target" attribute
+    //         const target = burger.dataset.target;
+    //         const $target = document.getElementById(target);
+    //         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    //         burger.classList.toggle("is-active");
+    //         $target.classList.toggle("is-active");
+    //       });
+    //     });
+    //   }
+    // });
+    //
+    // const $navbarLinks = Array.prototype.slice.call(
+    //   document.querySelectorAll(".navbar-menu div a"),
+    //   0
+    // );
+    // const $navMenu = $navbarLinks[0].parentElement.parentElement;
+    //
+    // $navbarLinks.forEach(link => {
+    //   link.addEventListener("click", () => {
+    //     const $burger = document.querySelector(".navbar-burger");
+    //
+    //     // Toggle the "is-active" class on burger & "navbar-menu"
+    //     $burger.classList.toggle("is-active");
+    //     $navMenu.classList.toggle("is-active");
+    //   });
+    // });
   }
   componentWillUnmount() {
-    window.removeEventListener("scroll", this.hideBar);
+    // window.removeEventListener("scroll", this.hideBar);
   }
   hideBar() {
-    let { isHide } = this.state;
-    window.scrollY > this.prev
-      ? !isHide && this.setState({ isHide: true })
-      : isHide && this.setState({ isHide: false });
-
-    this.prev = window.scrollY;
+    // let { isHide } = this.state;
+    // window.scrollY > this.prev
+    //   ? !isHide && this.setState({ isHide: true })
+    //   : isHide && this.setState({ isHide: false });
+    //
+    // this.prev = window.scrollY;
   }
 
   render() {
