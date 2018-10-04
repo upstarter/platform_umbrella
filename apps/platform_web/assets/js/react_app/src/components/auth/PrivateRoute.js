2ee4e48e243a315acb076c0140830d39ba7ee398
Eric Steen <ericsteen1@gmail.com>
2018-10-03 21:49:06 -0700

    Add auth

---
+++ apps/platform_web/assets/js/react_app/src/components/auth/PrivateRoute.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import fakeAuth from './fakeAuth'
+import {
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      fakeAuth.isAuthenticated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
+export default PrivateRoute
