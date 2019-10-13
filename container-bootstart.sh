fbb1a71b747b27049526c3c6e921bc77e4475c9d
Eric Steen <ericsteen1@gmail.com>
2019-10-12 21:25:33 -0700

    n

--- container-bootstart.sh
+++ container-bootstart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-./start_release foreground
+./start_release background
 ./cloud_sql_proxy -projects=eternal-sunset-206422 -dir=/tmp/cloudsql &
 /usr/local/bin/envoy -c /etc/api-envoy.yaml --service-cluster api --component-log-level "upstream:debug"
