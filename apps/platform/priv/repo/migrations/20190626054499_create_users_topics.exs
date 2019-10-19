5453681265afc2fea5ea959c7925fcc9f141a12b
Eric Steen <ericsteen1@gmail.com>
2019-10-19 09:57:45 -0700

    n

--- apps/platform/priv/repo/migrations/20190626054499_create_users_topics.exs
+++ apps/platform/priv/repo/migrations/20190626054499_create_users_topics.exs
@@ -2,7 +2,7 @@ defmodule Platform.Repo.Migrations.CreateUsersTopics do
   use Ecto.Migration
 
   def change do
-    create table(:users_topics, primary_key: false) do
+    create table(:users_topics) do
       add(:topic_id, references(:topics, on_delete: :delete_all), null: false)
       add(:user_id, references(:users, on_delete: :delete_all), null: false)
 
