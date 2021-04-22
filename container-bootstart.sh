1e5084c03267459d5ae067125f86f0104b52a1d1
Eric Steen <eric.steen@important.com>
2021-04-22 13:35:43 -0500

    more

--- container-bootstart.sh
+++ container-bootstart.sh
@@ -2,4 +2,4 @@
 set -ex
 ./start_release start
 ./cloud_sql_proxy -projects=eternal-sunset-206422 -dir=/tmp/cloudsql &
-/usr/local/bin/envoy -c /etc/api-envoy.yaml --service-cluster api --component-log-level "upstream:debug,connection:trace"
+/usr/local/bin/envoy -l off -c /etc/api-envoy.yaml --service-cluster api --component-log-level "upstream:debug,connection:trace"
