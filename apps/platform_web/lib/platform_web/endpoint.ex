d671b5343ea2954f59c4c48a1f77060140554d27
Eric Steen <eric.steen@important.com>
2021-04-22 11:55:30 -0500

    j

--- apps/platform_web/lib/platform_web/endpoint.ex
+++ apps/platform_web/lib/platform_web/endpoint.ex
@@ -50,7 +50,7 @@ defmodule PlatformWeb.Endpoint do
     max_age: 60 * 60
   )
 
-  plug(Corsica, origins: ~r{^https?://(.*.?)cryptowise.ai$})
+  plug(Corsica, origins: "*")
 
   plug(PlatformWeb.Router)
 
