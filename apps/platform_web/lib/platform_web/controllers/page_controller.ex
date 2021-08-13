8d9e35286aae8231ac091e9045af7ce3b6cfbb90
Eric Steen <eric.steen@important.com>
2021-08-13 17:02:38 -0500

    -v

--- apps/platform_web/lib/platform_web/controllers/page_controller.ex
+++ apps/platform_web/lib/platform_web/controllers/page_controller.ex
@@ -2,30 +2,30 @@ defmodule PlatformWeb.PageController do
   use PlatformWeb, :controller
 
   def home(conn, _params) do
-    render conn, "home.html"
+    render(conn, "home.html")
   end
 
   def test(conn, _params) do
-    render conn, "test.html"
+    render(conn, "test.html")
   end
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render(conn, "index.html")
   end
 
   def research(conn, _params) do
-    render conn, "research.html"
+    render(conn, "research.html")
   end
 
   def blog(conn, _params) do
-    render conn, "blog.html"
+    render(conn, "blog.html")
   end
 
   def press(conn, _params) do
-    render conn, "press.html"
+    render(conn, "press.html")
   end
 
   def privacy_policy(conn, _params) do
-    render conn, "privacy_policy.html"
+    render(conn, "privacy_policy.html")
   end
 end
